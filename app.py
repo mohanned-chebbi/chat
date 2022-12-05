23b12d82a62501be046ec93e181aa2988e9e78d6
mohanned-chebbi <mohanned.chebbi@gmail.com>
2022-12-05 21:40:08 +0100

    design sign-up done

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template , request, redirect
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'hello'
 
-@app.route('/signup')
+@app.route('/signup', methods = ['GET','POST'])
 def signup():
-    v="hodhod"
-    return render_template ('signup.html',h=v)
+    name = request.form.get('fname')
+    lastname = request.form.get('lname')
+    email = request.form.get('email')
+    passwd = request.form.get('password')
+    passwdc = request.form.get('passwordc')
+
+    gender = request.form.get('gender')
+    birth = request.form.get('birth')
+    nikname = request.form.get('nikname')
+
+    print (
+        'FName : ',name, '\n', 
+        'LName : ',lastname, '\n',
+        'email : ',email, '\n',
+        'pass : ',passwd, '\n',
+        'genre : ',gender, '\n',
+        'b_day : ',birth, '\n',
+        'pseudo : ',nikname, '\n'
+    )
+    #if request.method == 'POST':
+
+    return render_template ('signup.html')
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
@@ -15,6 +35,7 @@ def login():
     password = request.form.get('password') 
     print (email)
     return render_template ('login.html')
+
     
 @app.route('/chat')
 def chat():
