51f5c2cdd43705c9d5cedfc067f7b009530e1fe5
mohanned-chebbi <mohanned.chebbi@gmail.com>
2022-12-05 22:20:34 +0100

    connect to db with sign-up done

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
 from flask import Flask, render_template , request, redirect
+import mysql.connector
+import hashlib
+
+
 app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="chat-app"
+
+)
+
+print(mydb)
+
 @app.route('/')
 def hello_world():
     return 'hello'
 
 @app.route('/signup', methods = ['GET','POST'])
 def signup():
-    name = request.form.get('fname')
-    lastname = request.form.get('lname')
-    email = request.form.get('email')
-    passwd = request.form.get('password')
-    passwdc = request.form.get('passwordc')
+    if request.method == 'POST':
+        name = request.form.get('fname')
+        lastname = request.form.get('lname')
+        email = request.form.get('email')
+        passwd = request.form.get('password')
+        passwdc = request.form.get('passwordc')
 
-    gender = request.form.get('gender')
-    birth = request.form.get('birth')
-    nikname = request.form.get('nikname')
+        gender = request.form.get('gender')
+        birth = request.form.get('birth')
+        nikname = request.form.get('nikname')
 
-    print (
-        'FName : ',name, '\n', 
-        'LName : ',lastname, '\n',
-        'email : ',email, '\n',
-        'pass : ',passwd, '\n',
-        'genre : ',gender, '\n',
-        'b_day : ',birth, '\n',
-        'pseudo : ',nikname, '\n'
-    )
-    #if request.method == 'POST':
+        hash_pass = hashlib.md5(passwd.encode("utf-8")).hexdigest()
+        
+        mycursor = mydb.cursor()
+
+        sql = "INSERT INTO user (first_name, last_name, pseudo, email, passwd, gender, dateofbirth) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (name,lastname,nikname,email,hash_pass,gender,birth)
+        mycursor.execute(sql, val)
+        
+        mydb.commit()    
+        print(mycursor.rowcount, "record inserted.")
 
     return render_template ('signup.html')
 
